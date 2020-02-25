ff5f15e36e9d380b8b1400b158573a5d36ae280f
Jack McCoy <jackmccoy34@gmail.com>
2020-02-25 10:44:12 -0500

    Add project files.

---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(): base("DefaultCOnnection")
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
