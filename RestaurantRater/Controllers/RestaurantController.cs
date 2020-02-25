2eabee451b776c867365e7a1b8a4c2ce7d2612b2
Jack McCoy <jackmccoy34@gmail.com>
2020-02-25 14:44:40 -0500

    Restaurant Put Endpoint

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -17,6 +17,7 @@ namespace RestaurantRater.Controllers
 
 
         //POST
+        [HttpPost]
         public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
         {
             if (ModelState.IsValid && restaurant != null)
@@ -30,6 +31,7 @@ namespace RestaurantRater.Controllers
         }
 
         //GET ALL
+        [HttpGet]
         public async Task<IHttpActionResult> GetAll()
         {
             List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
@@ -37,6 +39,7 @@ namespace RestaurantRater.Controllers
         }
 
         //GET BY ID
+        [HttpGet]
         public async Task<IHttpActionResult> GetById(int id)
         {
             Restaurant restaurant = await _context.Restaurants.FindAsync(id);
@@ -49,7 +52,29 @@ namespace RestaurantRater.Controllers
         }
 
         //PUT (Update)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri]int id, [FromBody]Restaurant model)
+        {
+            if(ModelState.IsValid && model != null)
+            {
+                //This is our entity
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
 
+                if (restaurant != null)
+                {
+                    restaurant.Name = model.Name;
+                    restaurant.Style = model.Style;
+                    restaurant.Rating = model.Rating;
+                    restaurant.DollarSigns = model.DollarSigns;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
         //DELETE BY ID
+        //[HttpDelete]
     }
 }
